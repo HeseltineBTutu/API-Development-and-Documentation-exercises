2fe6abbfe2d4e519395a12fa8a9ae1dd93a74637
HeseltineBTutu <heseltinetutu@gmail.com>
2022-08-24 09:38:09 +0300

    Initial commit

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from models import setup_db, Plant
 from flask_cors import CORS
 
@@ -17,13 +17,25 @@ def create_app(test_config=None):
     @app.route('/plants')
     def get_plants():
         page = request.args.get('page', 1, type=int)
-        start = (page-1)*10
+        start = (page - 1) * 10
         end = start + 10
         plants = Plant.query.all()
         formatted_plants = [plant.format() for plant in plants]
         return jsonify({
             'success': True,
-            'plants': formatted_plants[start:end]
+            'plants': formatted_plants[start:end],
+            'total_plants': len(formatted_plants)
         })
+
+    @app.route('/plants/plants/<int:plant_id>')
+    def get_specific_plant(plant_id):
+        plant = Plant.query.filter(Plant.id == plant_id).one_or_none()
+        if plant is None:
+            abort(404)
+        else:
+            return jsonify({
+                'success': True,
+                'plant': plant.format()
+            })
 
     return app
